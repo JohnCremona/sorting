60bcfbb57eb54351d504b1371768387bb33d4eb0
AndrewVSutherland <drew@math.mit.edu>
2016-12-18 11:08:59 -0500

    Tweak magma interface, add sage/magma test

--- code/sorting.m
+++ code/sorting.m
@@ -2,19 +2,20 @@
     Magma code for comparing/sorting/enumerating ideals in the ring of integers OK of a number field.
     The ordering of ideals is described in detail in sorting.tex
     
-    IMPORTANT: the ring of integers OK used througout needs to be the ring of integers of a number field
-    that was created using the canonical defining polynomial (as listed in the LMFDB and returned by polredabs).
+    IMPORTANT: the field K should be created using the canonical defining polynomial (as listed in the LMFDB and returned by polredabs).
     
     Useful functions in this file:
     
-       SplitPrime(p,OK) --              sorted list of the prime OK-ideals dividing (p)
-       NumberOfIdealsOfNormQ(q,OK)      counts OK-ideals of prime power norm q
-       NumberOfIdealsOfNormN(n,OK)      counts OK-ideals of integer norm n
-       IdealsOfNormQ(q,OK)              sorted list of OK-ideals of prime power norm q
-       IdealsOfNormN(q,OK)              sorted list of OK-ideals of integer norm n
-       IdealOrdinal(I)                  returns m such that I is the mth ideal of its norm
-       IdealFromOrdinal(n,m,OK)         returns the mth OK-ideal of norm n
-       CompareIdeals(I,J)               returns -1,0,1 as I<J,I=J,I>J in our ordering
+       SplitPrime(K,p) --                   sorted list of the prime OK-ideals dividing (p)
+       NumberOfIdealsOfPrimePowerNorm(K,q)  counts OK-ideals of prime power norm q
+       NumberOfIdealsOfNorm(K,n)            counts OK-ideals of integer norm n
+       IdealsOfPrimePowerNorm(q,OK)         sorted list of OK-ideals of prime power norm q
+       IdealsOfNorm(K,q)                    sorted list of OK-ideals of integer norm n
+       IdealNormIndex(I)                    returns m such that I is the mth ideal of its norm
+       IdealFromNormIndex(K,n,m)            returns the mth OK-ideal of norm n
+       IdealLabel(I)                        returns a text label of the form n.m with n=norm, m=index
+       IdealFromLabel(K,label)              returns the OK-ideal with the specified label
+       CompareIdeals(I,J)                   returns -1,0,1 as I<J,I=J,I>J in our ordering
 */
 
 function UniqueMax(V)
@@ -52,7 +53,8 @@ end function;
 // The list is orderd by norm, ramification index, pAdic-poly, where the pAdic-poly h corresponding to a prime ideal P is the unique
 // irreducible factor of g in Zp[x] for which v_P(h(a)) is infinite.  pAdic polys h1 and h2 of the same degree are ordered
 // lexicographically according to the p-adic expansions of their coeffs (written in base p with digits in [0,p-1]).
-function SplitPrime(p,OK)
+function SplitPrime(K,p)
+    OK:=RingOfIntegers(K);
     R:=PolynomialRing(Integers());
     p:=Integers()!p;
     error if not IsPrime(p), Sprintf("Input p = %o to SplitPrime is not prime",p);
@@ -60,9 +62,8 @@ function SplitPrime(p,OK)
     g:=DefiningPolynomial(OK);
     P:=[s[1]:s in Factorization(ideal<OK|p>)];          // prime ideals dividing p
     if #P eq 1 then return P; end if;                   // if there is only one prime above p, we are done.
-    a:=OK.2;
+    a:=K.1;
     g:=R!MinimalPolynomial(a);
-    assert g eq DefiningPolynomial(OK);
     k := 1;
     while true do
         // compute distinct factors of f in Zp[x]/(p^kZ_p[x])
@@ -100,20 +101,21 @@ end function;
 
 // Counts the number of ideals of prime power norm q in the ring of integers OK
 // This is faster than actually enumerating them all
-function NumberOfIdealsOfNormQ(q,OK)
+function NumberOfIdealsOfPrimePowerNorm(K,q)
+    OK:=RingOfIntegers(K);
     q:=Integers()!q;
     b,p,k := IsPrimePower(q);
-    error if not b, Sprintf("Input q = %o to NumberOfIdealsOfNormQ is not a prime power.", q);
+    error if not b, Sprintf("Input q = %o to NumberOfIdealsOfPrimePowerNorm is not a prime power.", q);
     V:=[InertiaDegree(a[1]):a in Factorization(ideal<OK|p>)];
     return NumberOfPoints(Polytope([[i eq j select k/V[i] else 0:i in [1..#V]]:j in [1..#V]]));
 end function;
 
 // Counts the number of ideals of integer norm n in the number field Q[x]/(g(x))
 // This is substantially faster than actually enumerating them all
-function NumberOfIdealsOfNormN(n,OK)
+function NumberOfIdealsOfNorm(K,n)
     n:=Integers()!n;
     P:=Factorization(n);
-    return &*[NumberOfIdealsOfNormQ(p[1]^p[2],OK):p in P];
+    return &*[NumberOfIdealsOfPrimePowerNorm(K,p[1]^p[2]):p in P];
 end function;
 
 // We compare exponent vectors of prime-power norm ideals first by weight (sum) and then
@@ -130,11 +132,11 @@ function CompareExponentVectors(v,w)
 end function;
 
 // Returns a list of ideals of prime power norm q in the number field Q[x]/(g(x)) ordered by exponent vector (weight, reverse lex)
-function IdealsOfNormQ(q,OK)
+function IdealsOfPrimePowerNorm(K,q)
     q:=Integers()!q;
     b,p,k := IsPrimePower(q);
-    error if not b, Sprintf("Input q = %o to IdealsOfNormQ is not a prime power.", q);
-    P:=SplitPrime(p,OK);
+    error if not b, Sprintf("Input q = %o to IdealsOfPrimePowerNorm is not a prime power.", q);
+    P:=SplitPrime(K,p);
     V:=[InertiaDegree(I):I in P];
     S:=[[Vector(v)[i]:i in [1..#V]]:v in Points(Polytope([[i eq j select k/V[i] else 0:i in [1..#V]]:j in [1..#V]]))];
     S:=Sort(S,CompareExponentVectors);
@@ -143,38 +145,44 @@ end function;
 
 // Returns a list of ideals of integer norm n in the number field Q[x]/(g(x)) lexicographically ordered according to their factorizations
 // into ideals of prime power norm, where ideals of the same prime power norm are ordered by exponent vector (weight, reverse lex)
-function IdealsOfNormN(n,OK)
+function IdealsOfNorm(K,n)
+    OK:=RingOfIntegers(K);
     n:=Integers()!n;
     if n eq 1 then return ideal<OK|1>; end if;
     pp:=[p[1]^p[2]:p in Factorization(n)];
-    Q:=<IdealsOfNormQ(q,OK):q in pp>;
+    Q:=<IdealsOfPrimePowerNorm(K,q):q in pp>;
     return [&*[I:I in t]:t in CartesianProduct(Q)];
 end function;
 
 // Given an OK-ideal I returns the integer m for which I is the mth OK-ideal of norm N(I)
-function IdealOrdinal(I)
+function IdealNormIndex(I)
     n:=Norm(I);
     if n eq 1 then return 1; end if;
     pp:=[p[1]^p[2]:p in Factorization(n)];
-    Q:=<IdealsOfNormQ(q,Order(I)):q in pp>;
+    Q:=<IdealsOfPrimePowerNorm(NumberField(Order(I)),q):q in pp>;
     A:=<GCD(ideal<Order(I)|q>,I):q in pp>;
     v:=<Index(Q[i],A[i]):i in [1..#pp]>;
     w:=Append([&*[#Q[i]: i in [j+1..#pp]]:j in [1..#pp-1]],1);
     return 1+&+[(v[i]-1)*w[i]:i in [1..#pp]];
 end function;
 
-// Given integers n and m and a ring of integers OK, returns the mth OK-ideal of norm n
+function IdealLabel(I)
+    return Sprintf("%o.%o",Norm(I),IdealNormIndex(I));
+end function;
+
+// Given integers n and m, returns the mth OK-ideal of norm n for the specified field K
 // Returns 0 if there are no OK-ideals of norm n
-function IdealFromOrdinal(n,m,OK)
-    n:=Integers()!n; m:=Integers()!m;
-    error if n le 0, Sprintf("Invalid norm %o in IdealFromOrdinal",n);
+function IdealFromNormIndex(K,n,m)
+    error if n le 0, Sprintf("Invalid norm %o in IdealFromNormIndex, expected a positive integer",n);
+    error if m le 0, Sprintf("Invalid index %o in IdealFromNormIndex, expected a positive integer",n);
+    OK:=RingOfIntegers(K);
     I:=ideal<OK|1>;
     if n eq 1 then
-        error if m ne 1, Sprintf("Invalid ordinal %o for norm %o in IdealFromOrdinal", m,n);
+        error if m ne 1, Sprintf("No ideal with norm %o and index %o exists in the number field %o", n, m, K);
         return I;
     end if;
     pp:=[p[1]^p[2]:p in Factorization(n)];
-    Q:=<IdealsOfNormQ(q,OK):q in pp>;
+    Q:=<IdealsOfPrimePowerNorm(K,q):q in pp>;
     for S in Q do if #S eq 0 then return 0; end if; end for;
     a := m-1;
     for j:=#Q to 1 by -1 do
@@ -182,10 +190,17 @@ function IdealFromOrdinal(n,m,OK)
         I *:= Q[j][i+1];
         a:=ExactQuotient(a-i,#Q[j]);
     end for;
-    error if a ne 0, Sprintf("Invalid ordinal %o for norm %o in IdealFromOrdinal", m,n);
+    error if a ne 0, Sprintf("No ideal with  norm %o and index %o exists in the number field %o", n, m, K);
     return I;
 end function;
 
+function IdealFromLabel(K,label)
+    s:=Split(label,".");
+    error if #s ne 2, "Invalid ideal label %o in IdealFromLabel, should be of the form n.m where n is the norm and m is the index", label;
+    n:=StringToInteger(s[1]); m:=StringToInteger(s[2]);
+    return IdealFromNormIndex(K,n,m);
+end function;
+    
 function CompareIdeals(I1,I2)
     n1:=Norm(I1); n2:=Norm(I2);
     if n1 lt n2 then return -1; end if;
@@ -194,7 +209,7 @@ function CompareIdeals(I1,I2)
     for q in pp do
         J1:=GCD(I1,ideal<Order(I1)|q>); J2:=GCD(I2,ideal<Order(I2)|q>);
         if J1 ne J2 then
-            Q:=IdealsOfNormQ(q,Order(I1));
+            Q:=IdealsOfPrimePowerNorm(NumberField(Order(I1)),q);
             if Index(Q,J1) lt Index(Q,J2) then return -1; else return 1; end if;
         end if;
     end for;
@@ -202,19 +217,19 @@ function CompareIdeals(I1,I2)
 end function;
 
 // test function that exercises some of the code above
-procedure Test(OK,N)
+procedure Test(K,N)
     cnt:=0;
     for n:=2 to N do
-        S:=IdealsOfNormN(n,OK);
-        for I in S do
-            m:=IdealOrdinal(I);
-            printf "%o.%o\n",n,m;
-            assert I eq IdealFromOrdinal(n,m,OK);
-            if I ne S[#S] then
-                assert CompareIdeals(I,IdealFromOrdinal(n,m+1,OK)) eq -1;
+        S:=IdealsOfNorm(K,n);
+        for m:=1 to #S do
+            label := IdealLabel(S[m]);
+            print label;
+            assert S[m] eq IdealFromLabel(K,label);
+            if m lt #S then
+                assert CompareIdeals(S[m],IdealFromNormIndex(K,n,m+1)) eq -1;
             end if;
             cnt+:=1;
         end for;
     end for;
-    printf "Successfully checked %o non-trivial ideals of norm up to %o in %o\n", cnt, N, OK;
+    printf "Successfully checked %o non-trivial ideals of norm up to %o in the field %o\n", cnt, N, K;
 end procedure;
